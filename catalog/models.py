33aff4c2ab2cb91926411a7a3cf2bdaff3fde23b
HAN <han82@mail.ru>
2023-12-19 04:04:48 +0300

    commit 20,1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+    preview = models.ImageField(upload_to='preview/', **NULLABLE, verbose_name='Изображение')
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    create_date = models.DateField(verbose_name='Дата создания')
+    last_change = models.DateField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.category} {self.price} {self.create_date} {self.last_change}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(max_length=1000, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
